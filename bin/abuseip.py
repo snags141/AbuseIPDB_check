f096b40732cc536160a5e2369bdb273806b9412f
Daniel Harvey <dan@harvey5.net>
2020-03-03 06:26:45 +0800

    Initial commit

---
+++ bin/abuseip.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import requests as req
+import json
+from splunklib.searchcommands import dispatch, StreamingCommand, Configuration, Option, validators
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
+from splunklib.searchcommands import \
+    dispatch, StreamingCommand, Configuration, Option, validators
+
+
+@Configuration()
+class abuseipCommand(StreamingCommand):
+
+    """ %(synopsis)
+
+    ##Syntax
+
+    %(syntax)
+
+    ##Description
+
+    %(description)
+
+    """
+
+    ipfield = Option(
+        doc='''
+        **Syntax:** **ipfield=***<fieldname>*
+        **Description:** Name of the IP address field to look up''',
+        require=True, validate=validators.Fieldname())
+
+    def stream(self, events):
+        # Load config with user specified API key, if this file does not exist copy it from ../default
+        with open('../local/config.json') as config_file:
+            data = json.load(config_file)
+            api_key = data['abuseip'][0]['api_key']
+
+        # API required headers
+        headers = {
+            'Key': api_key,
+            'Accept': 'application/json',
+        }
+
+
+        for event in events:
+            event_dest_ip = event[self.ipfield]
+            # API required parameters
+            params = (
+                ('ipAddress', event_dest_ip),
+                ('maxAgeInDays', '90'),
+                ('verbose', ''),
+            )
+            # Make API Request
+            response = req.get('https://api.abuseipdb.com/api/v2/check', headers=headers, params=params)
+            data=response.json()
+            addr_country_name = data['data']['countryName']
+            addr_domain = data['data']['domain']
+            addr_isp = data['data']['isp']
+            addr_last_reported = data['data']['lastReportedAt']
+            addr_abuse_confidence = data['data']['abuseConfidenceScore']
+
+            # Set event values to be returned
+            event["CountryName"] = addr_country_name
+            event["Domain"] = addr_domain
+            event["ISP"] = addr_isp
+            event["LastReportedAt"] = addr_last_reported
+            event["AbuseConfidence"] = addr_abuse_confidence
+
+            # Finalize event
+            yield event
+
+
+dispatch(abuseipCommand, sys.argv, sys.stdin, sys.stdout, __name__)
